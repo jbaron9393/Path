ee845ba7b66af2889b4f14c3ae6b5fd075bb368d
James Baron <james.baron@cuanschutz.edu>
2026-09-09 16:17:41 -0600

    Use v4 startup pipeline locally

--- start_cloze_refiner.bat
+++ start_cloze_refiner.bat
@@ -4,8 +4,8 @@ cd /d "%~dp0"
 REM Kill any old node servers silently
 taskkill /F /IM node.exe >nul 2>&1
 
-REM Start fresh
-start "" /B node server.js
+REM Start fresh with the integrated v4 cloze pipeline
+start "" /B node scripts/run-v4.mjs
 
 REM Small wait
 ping 127.0.0.1 -n 2 >nul
